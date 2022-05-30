fa63a7ed98085b4197e395efa49c4c17132f1f7d
Florian COURTAIGNE <f.courtaigne@gmail.com>
2022-05-30 17:29:14 +0200

    working on findby category and template

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,8 +2,10 @@
 // src/Controller/CategoryController.php
 namespace App\Controller;
 
+use App\Entity\Program;
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +16,7 @@ class CategoryController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index(CategoryRepository $categoryRepository): Response
-    {
+    {   
         $categories = $categoryRepository->findAll();
         return $this->render('category/index.html.twig', [
             'website' => 'Wild Series',
@@ -22,21 +24,22 @@ class CategoryController extends AbstractController
         ]);
     }
 
-    #[Route('/category/{categoryName}', name: 'show')]
-    public function show(string $categoryName, Program $programInCategory) : Response
+    #[Route('/{categoryName}', name: 'show')]
+    public function findProgramByCategory(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
     {
-        $category = $categoryRepository->findBy(['categoryName' => $categoryName]);
-        
-        if (!$category) {
-            throw $this->createNotFoundException(
-                'No category with name : '.$categoryName .' found in category\'s table.'
-            );
-        } else {
-            $programByCategory = $programInCategory->getTitle();
+            $category = $categoryRepository->findOneByName($categoryName);
+            $programs = $programRepository->findByCategory($category);
+            
+            if (!$category) {
+                throw $this->createNotFoundException(
+                    'No category with name : '.$categoryName .' found in category\'s table.'
+                );
+            } else {
+            return $this->render('category/show.html.twig', [
+                'website' => 'Wild Series',
+                'programs' => $programs,
+                'category' => $category,
+            ]);
         }
-
-        return $this->render('category/show.html.twig', [
-            'categoryName' => $categoryName, 
-    ]);
     }
 }
